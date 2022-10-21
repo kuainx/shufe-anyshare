4f36ed6bbb0de4d0a4a2596a4d24b9095c3c2153
KUAI <2542862169@qq.com>
2022-10-21 20:16:30 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shufe-anyshare
 shufe anyshare download
+
+[nilaoda/N_m3u8DL-CLI: [.NET\] m3u8 downloader 开源的命令行m3u8/HLS/dash下载器，支持普通AES-128-CBC解密，多线程，自定义请求头等. 支持简体中文,繁体中文和英文. English Supported. (github.com)](https://github.com/nilaoda/N_m3u8DL-CLI)
+
---
+++ anyshare.js
@@ -0,0 +1,42 @@
+(async function () {
+  const postJson = async (url, data) => {
+    const ret = await $.post(url, JSON.stringify(data))
+    return JSON.parse(ret)
+  }
+  const processDir = async ({ dirs, files }, dirname) => {
+    let RET = "文件夹{" + dirname + "}下有" + files.length + "文件\n"
+    for (let i = 0; i < files.length; i++) {
+      RET += await getDownloadUrl(files[i])
+    }
+    RET += "文件夹{" + dirname + "}下有" + dirs.length + "文件夹\n"
+    for (let i = 0; i < dirs.length; i++) {
+      const dirInfo = await listDir(dirs[i].docid)
+      RET += await processDir(dirInfo, dirname + "/" + dirs[i].name)
+    }
+    return RET
+  }
+  const getDownloadUrl = async ({ docid, rev, name }) => {
+    const playinfo = await postJson("https://anyshare.sufe.edu.cn:9124/v1/link?method=playinfo", {
+      docid,
+      link: currentHash,
+      password: baseLink.password,
+      rev,
+    })
+    return `"https://anyshare.sufe.edu.cn:9124/v1/link?method=play&docid=${playinfo.docid}&reqhost=anyshare.sufe.edu.cn&usehttps=true" --saveName "${name}"\n`
+  }
+  const listDir = async (docid) => {
+    return await postJson("https://anyshare.sufe.edu.cn:9124/v1/link?method=listdir", {
+      by: "name",
+      sort: "asc",
+      link: currentHash,
+      password: baseLink.password,
+      docid
+    })
+  }
+  const currentHash = location.hash.split("?")[0].split("/")[2]
+  const baseLink = session.get("link")[currentHash]
+  const baseDir = await listDir(baseLink.docid)
+  let OUTPUT = "根目录：" + baseLink.name + "|" + baseLink.usrdisplayname + "|" + baseLink.usrloginname + "\n"
+  OUTPUT += await processDir(baseDir, baseLink.name)
+  console.log(OUTPUT);
+})()
